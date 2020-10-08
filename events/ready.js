42304c7cd171254c01ee5038b20dc8ba7072631e
Bronems <k.galus@icloud.com>
2020-10-08 20:11:48 +0200

    1.0

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
     client.user.setActivity("I'm Botty !", {type : 'LISTENING'});
     const channel = client.channels.cache.find(r => r.name === "logs");
-    channel.send("<a:certif:738869068680921158> Bot is ready");
+    channel.send(":wave: Bot is ready");
     console.log("Ready");
 }  
