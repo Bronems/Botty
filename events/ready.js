3677ec91708a46795590e3228ad938835f2dcc30
Bronems <k.galus@icloud.com>
2020-10-08 20:41:16 +0200

    Remove start message

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,4 @@
 module.exports = (client) => {
     client.user.setActivity("I'm Botty !", {type : 'LISTENING'});
-    const channel = client.channels.cache.find(r => r.name === "logs");
-    channel.send(":wave: Bot is ready");
     console.log("Ready");
 }  
